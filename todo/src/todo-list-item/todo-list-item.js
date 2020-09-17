b72c04619b22ef101c528ad7f9722525b023c3d8
SLain <sl163@ya.ru>
2020-09-17 14:04:33 +0300

    Добавляет в скрипты события для кнопок done и alarm;

--- todo/src/todo-list-item/todo-list-item.js
+++ todo/src/todo-list-item/todo-list-item.js
@@ -4,18 +4,59 @@ import { faExclamation, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import './todo-list-item.css';
 
 class TodoListItem extends Component {
-    constructor({taskName, alarm = false}) {
-        super()
-        this.itemStyle = alarm ? 'todo-item__text todo-item_like' : 'todo-item__text';
-        this.taskName = taskName;
+    state = {
+        done: false,
+        alarm: false
+    };
+
+    onAlarm = () => {
+        this.setState((state) => {
+            return {
+                alarm: !state.alarm
+            }
+        })
+    };
+
+    onDone = () => {
+        this.setState((state) => {
+            return {
+                done: !state.done
+            }
+        })
     }
+
     render() {
+        const {taskName} = this.props;
+        const {done, alarm} = this.state;
+        let textStyle = 'todo-item__text';
+        let alarmBtnStyle = 'todo-item__alarm todo-item__icon';
+
+        if (alarm) {
+            textStyle += ' todo-item_alarm';
+            alarmBtnStyle += ' todo-item_inactive';
+        }
+
+        if(done) {
+            textStyle += ' todo-item_done';
+        }
+
         return (
             <div className="todo-item">
-                <span className={this.itemStyle}>{this.taskName}</span>
+                <span 
+                className={textStyle}
+                onClick={this.onDone}>
+                    {taskName}
+                </span>
                 <div className="todo-item__icon-block">
-                    <span className="todo-item__star todo-item__icon"><FontAwesomeIcon icon={faExclamation}/></span>
-                    <span className="todo-item__bin todo-item__icon"><FontAwesomeIcon icon={faTrashAlt}/></span>
+                    <span 
+                    className={alarmBtnStyle}
+                    onClick={this.onAlarm}>
+                        <FontAwesomeIcon icon={faExclamation}/>
+                    </span>
+                    <span 
+                    className="todo-item__bin todo-item__icon">
+                        <FontAwesomeIcon icon={faTrashAlt}/>
+                    </span>
                 </div>
             </div>
         )
