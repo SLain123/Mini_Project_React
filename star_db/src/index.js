df94db9c2f1f399c6d12e4d04262761b5735c7b0
SLain <sl163@ya.ru>
2020-09-20 11:37:45 +0300

    Добавляет рабочую функцию получения данных с сервера через fetch;

--- star_db/src/index.js
+++ star_db/src/index.js
@@ -1,1 +1,19 @@
-console.log('start');
+const getData = async url => {
+  const request = await fetch(url);
+
+  if(!request.ok) {
+    throw new Error(`Ошибка при загрузке ${request.url}: ${request.status}`);
+  }
+
+  const data = await request.json();
+
+  return data;
+}
+
+getData('https://swapi.dev/api/people/11/')
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(`Сервер недоступен: ${error}`);
+  })
