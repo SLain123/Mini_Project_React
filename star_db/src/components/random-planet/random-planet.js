dc85a8f14838c4de5c2ddcfa862e904c3cd2dd6a
SLain <sl163@ya.ru>
2020-09-21 16:27:44 +0300

    Добавляет рабочую версию скриптов для отображения рандомных планет в верхнем блоке;

--- star_db/src/components/random-planet/random-planet.js
+++ star_db/src/components/random-planet/random-planet.js
@@ -1,22 +1,44 @@
 import React, {Component} from 'react';
+import LoadData from '../../services/load-data-service';
 import './random-planet.css';
 
 class RandomPlanet extends Component {
+    constructor() {
+        super()
+        this._random = (Math.floor(Math.random() * 19)) + 1;
+        this.updatePlanet();
+    }
+
+    allData = new LoadData();
+
+    state = {}
+
+    updatePlanet() {
+        const id = this._random;
+        this.allData.getUnit('planets', id).then(data => {
+            this.setState(data)
+        })
+    }
+
     render() {
+        const {id = this._random, name, population, rotationPeriod, diameter, orbitalPeriod, gravity, climate} = this.state;
         return (
             <div className="random-planet random-planet_pos">
                 <img 
                     className="random-planet__pic"
-                    src="https://img1.goodfon.ru/original/640x480/6/61/art-deathcl0ck-kosmos-planeta.jpg"
+                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
                     alt="planet"
                     width="300"
                     height="300"/>
                 <div className="random-planet__info-block">
-                    <h1 className="random-planet__info-title">Planet</h1>
+                    <h1 className="random-planet__info-title">{name}</h1>
                     <ul className="random-planet__info-detals-list">
-                        <li className="random-planet__info-details-item">population: 78577557</li>
-                        <li className="random-planet__info-details-item">rotation: 3454646</li>
-                        <li className="random-planet__info-details-item">diametr: 77757</li>
+                        <li className="random-planet__info-details-item">Population: {population}</li>
+                        <li className="random-planet__info-details-item">Rotation Period: {rotationPeriod}</li>
+                        <li className="random-planet__info-details-item">Diameter: {diameter}</li>
+                        <li className="random-planet__info-details-item">Orbital Period: {orbitalPeriod}</li>
+                        <li className="random-planet__info-details-item">Gravity: {gravity}</li>
+                        <li className="random-planet__info-details-item">Climate: {climate}</li>
                     </ul>
                 </div>
             </div>
