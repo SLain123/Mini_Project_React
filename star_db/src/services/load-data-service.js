e6169008793c02bb583e0d49776594ecad5861e6
SLain <sl163@ya.ru>
2020-09-21 16:26:15 +0300

    Вносит модификацию в сервис получения данных для единичных объектов: Переработка скрипта с добавлением внутренней трансформации получаемых данных до формата используемого в проекте;

--- star_db/src/services/load-data-service.js
+++ star_db/src/services/load-data-service.js
@@ -1,7 +1,7 @@
 class LoadData {
     _swapi = 'https://swapi.dev/api/';
 
-    async getData(url) {
+    async _getData(url) {
         const request = await fetch(`${this._swapi}${url}`);
         if (!request.ok) {
             throw new Error(`Ошибка при загрузке ${request.url}: ${request.status}`);
@@ -12,15 +12,50 @@ class LoadData {
     }
 
     getAllUnit(unit) {
-        return this.getData(`${unit}/`).then(data => {
+        return this._getData(`${unit}/`).then(data => {
             return data.results;
         })
     }
 
     getUnit(unit, id) {
-        return this.getData(`${unit}/${id}`).then(data => {
-            return data;
-        })
+        return this._getData(`${unit}/${id}`).then(data => {
+                switch(unit) {
+                    case 'planets': 
+                    return {
+                        id,
+                        name: data.name,
+                        population: data.population,
+                        rotationPeriod: data.rotation_period,
+                        diameter: data.diameter,
+                        orbitalPeriod: data.orbital_period,
+                        gravity: data.gravity,
+                        climate: data.climate
+                    };
+                    case 'people': 
+                    return {
+                        id,
+                        name: data.name,
+                        population: data.population,
+                        rotationPeriod: data.rotation_period,
+                        diameter: data.diameter,
+                        orbitalPeriod: data.orbital_period,
+                        gravity: data.gravity,
+                        climate: data.climate
+                    };
+                    case 'starships': 
+                    return {
+                        id,
+                        name: data.name,
+                        population: data.population,
+                        rotationPeriod: data.rotation_period,
+                        diameter: data.diameter,
+                        orbitalPeriod: data.orbital_period,
+                        gravity: data.gravity,
+                        climate: data.climate
+                    };
+                    default: return 'No exist data type';
+                }
+            })
     }
 }
 
