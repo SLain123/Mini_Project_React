a580b0c5e442cabf92b721a5f7eb7c154a1a01f7
SLain <sl163@ya.ru>
2020-11-03 09:55:57 +0300

    Добавляет react-route-dom; Добавляет титул по URL /; Добавляет конкретный URL путь к каждому информационному разделу для последующего рендеринга на странице;

--- star_db/src/components/app/app.js
+++ star_db/src/components/app/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import Header from '../header/header';
 import RandomPlanet from '../random-planet/random-planet';
 import UnitPage from '../unit-page/unit-page';
@@ -10,62 +11,63 @@ import './app.css';
 const App = () => {
 
     const peopleStore = {
-            gender: "Gender",
-            height: "Height",
-            mass: "Mass",
-            birthYear: "Birth year",
-            eyeColor: "Eye color",
-            hairColor: "Hair color",
-            skinColor: "Skin color"
-        };
+        gender: "Gender",
+        height: "Height",
+        mass: "Mass",
+        birthYear: "Birth year",
+        eyeColor: "Eye color",
+        hairColor: "Hair color",
+        skinColor: "Skin color"
+    };
 
     const planetsStore = {
-            population: "Population", 
-            rotationPeriod: 'Rotation period', 
-            diameter: 'Diameter', 
-            orbitalPeriod: 'Orbital period', 
-            gravity: 'Gravity', 
-            climate: 'Climate',
-            terrain: 'Terrain'
-        };
+        population: "Population",
+        rotationPeriod: 'Rotation period',
+        diameter: 'Diameter',
+        orbitalPeriod: 'Orbital period',
+        gravity: 'Gravity',
+        climate: 'Climate',
+        terrain: 'Terrain'
+    };
 
     const starshipsStore = {
-            model: 'Model',
-            cargoCapacity:'Cargo capacity',
-            consumables: 'Consumables',
-            cost: 'Cost incredits',
-            hyperDriveRating: 'Hyperdrive rating',
-            length: 'Length',
-            maxAtmospheringSpeed: 'Max atmosphering speed',
-            passengers: 'Passengers',
-            starshipClass: 'Starship class'
-        }
+        model: 'Model',
+        cargoCapacity: 'Cargo capacity',
+        consumables: 'Consumables',
+        cost: 'Cost incredits',
+        hyperDriveRating: 'Hyperdrive rating',
+        length: 'Length',
+        maxAtmospheringSpeed: 'Max atmosphering speed',
+        passengers: 'Passengers',
+        starshipClass: 'Starship class'
+    }
 
     return (
         <ErrorBoundy>
                 <div className="main">
-                    <Header/>
+                    <Header />
                     <ErrorBoundy>
                         <section className="top-part">
-                            <RandomPlanet/>
+                            <RandomPlanet />
                         </section>
                     </ErrorBoundy>
-                    <TestError/>
+                    <TestError />
                     <section className="bottom-part">
                         <ErrorBoundy>
-                            <UnitPage 
-                                request="people"
-                                data={peopleStore}/>
-                        </ErrorBoundy>
-                        <ErrorBoundy>
-                            <UnitPage 
-                                request="planets"
-                                data={planetsStore}/>
-                        </ErrorBoundy>
-                        <ErrorBoundy>
-                            <UnitPage 
-                                request="starships"
-                                data={starshipsStore}/>
+                            <Switch>
+                                <Route path='/' render={
+                                    () => <h1 className="main-title">Select the appropriate section in the <span className="main-title_green">HEADER</span> above to get information</h1>
+                                    } />
+                                <Route path='/people' component={
+                                    () => <UnitPage request="people" data={peopleStore} />
+                                    } />
+                                <Route path='/planets' component={
+                                    () => <UnitPage request="planets" data={planetsStore} />
+                                } />
+                                <Route path='/starships' component={
+                                    () => <UnitPage request="starships" data={starshipsStore} />
+                                } />
+                            </Switch>
                         </ErrorBoundy>
                     </section>
                 </div>
