2d1962a59fad7a167aba772309ea582537cd0a19
SLain <sl163@ya.ru>
2021-01-26 16:30:17 +0300

    Добавляет стили для Search; Добавляет функционал поиска фильмов по названию через Search; Подключает debounce из lodash;

--- moviesApp/src/components/app/App.jsx
+++ moviesApp/src/components/app/App.jsx
@@ -1,53 +1,96 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unused-state */
-/* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 import { Spin, Alert } from "antd";
+import * as _ from "lodash";
+import Search from "../search";
 import MovieService from "../../services/movies-service";
 import MovieList from "../movie-list";
+
+const MainContent = ({
+  movieListArr,
+  searchWord,
+  changeSearchWord,
+  onLoad,
+  onFail,
+}) => {
+  if (onLoad) {
+    return <Spin tip="Loading..." size="large" />;
+  }
+  if (!onLoad && onFail) {
+    return (
+      <Alert
+        type="error"
+        message={`Error: Can't download data! ${onFail}`}
+        description="An error occurred while downloading data from a remote server"
+        banner
+      />
+    );
+  }
+  return (
+    <>
+      <Search searchWord={searchWord} changeSearchWord={changeSearchWord} />
+      <MovieList movieListArr={movieListArr} />
+    </>
+  );
+};
 
 class App extends Component {
   state = {
     movieListArr: [],
     onLoad: true,
     onFail: false,
+    searchWord: "",
   };
 
-  componentDidMount() {
-    setTimeout(() => {
-      MovieService.getMoviesByTitle("return")
-        .then(({ results }) => {
-          this.setState({
-            movieListArr: results,
-            onLoad: false,
-          });
-        })
-        .catch((error) => {
-          this.setState({
-            onFail: error,
-            onLoad: false,
-          });
+  updateMovieList = _.debounce((searchWord) => {
+    MovieService.getMoviesByTitle(searchWord)
+      .then(({ results }) => {
+        this.setState({
+          movieListArr: results,
+          onLoad: false,
         });
-    }, 1000);
+      })
+      .catch((error) => {
+        this.setState({
+          onFail: error,
+          onLoad: false,
+        });
+      });
+  }, 500);
+
+  componentDidMount() {
+    this.updateMovieList("return");
   }
 
-  render() {
-    const { movieListArr, onLoad, onFail } = this.state;
-    let content = <MovieList movieListArr={movieListArr} />;
-
-    if (onLoad) {
-      content = <Spin tip="Loading..." size="large" />;
-    } else if (!onLoad && onFail) {
-      content = (
-        <Alert
-          type="error"
-          message={`Error: Can't download data! ${onFail}`}
-          description="An error occurred while downloading data from a remote server"
-          banner
-        />
-      );
+  componentDidUpdate(prevProps, prevState) {
+    const { searchWord } = this.state;
+    if (prevState.searchWord !== searchWord && searchWord) {
+      this.updateMovieList(searchWord);
     }
+  }
 
-    return <div className="app">{content}</div>;
+  changeSearchWord = (searchWord) => {
+    this.setState({
+      searchWord,
+    });
+  };
+
+  render() {
+    const { movieListArr, onLoad, onFail, searchWord } = this.state;
+
+    return (
+      <div className="app">
+        <MainContent
+          movieListArr={movieListArr}
+          searchWord={searchWord}
+          changeSearchWord={this.changeSearchWord}
+          onLoad={onLoad}
+          onFail={onFail}
+        />
+      </div>
+    );
   }
 }
 
--- moviesApp/src/components/search/Search.jsx
+++ moviesApp/src/components/search/Search.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Search = () => <input type="input" placeholder="Type to search..." />;
+const Search = ({ searchWord, changeSearchWord }) => (
+  <input
+    className="search"
+    type="input"
+    placeholder="Type to search..."
+    value={searchWord}
+    onChange={(evt) => changeSearchWord(evt.target.value)}
+  />
+);
+
+Search.propTypes = {
+  searchWord: PropTypes.string.isRequired,
+  changeSearchWord: PropTypes.func.isRequired,
+};
 
 export default Search;
