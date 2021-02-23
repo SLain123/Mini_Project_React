94bf696cba22f8da5bf1562e27a4ebe5969efd16
SLain <sl163@ya.ru>
2021-02-23 12:58:10 +0300

    Добавляет отображаение интерфейса приложения при потере интернет соединения; Исправляет текст ошибки в компоненте ErrorMessage;

--- moviesApp/src/components/app/App.jsx
+++ moviesApp/src/components/app/App.jsx
@@ -1,139 +1,144 @@
-import React, { Component } from "react";
-import * as _ from "lodash";
-import { Spin } from "antd";
-import ErrorMessage from "../errorMessage";
-import Tabs from "../tabs";
-import Search from "../search";
-import MovieService from "../../services/movies-service";
-import MovieList from "../movie-list";
-import { ContextProvider } from "../../services/ContextProvider";
+import React, { Component } from 'react';
+import * as _ from 'lodash';
+import { Spin } from 'antd';
+import ErrorMessage from '../errorMessage';
+import Tabs from '../tabs';
+import Search from '../search';
+import MovieService from '../../services/movies-service';
+import MovieList from '../movie-list';
+import { ContextProvider } from '../../services/ContextProvider';
 
 class App extends Component {
-  state = {
-    movieSearchList: [],
-    onloadingSearch: true,
-    onFailSearch: false,
-    searchWord: "",
-    page: 1,
-    totalResults: null,
-    scroll: false,
-  };
-
-  updateMovieList = _.debounce((searchWord, searchPage) => {
-    if(searchWord !== '' && searchWord.match(/[\S]/) !== null) {
-      MovieService.getMoviesByTitle(searchWord, searchPage)
-      .then(({ results, total_results: totalResults, page }) => {
-        this.setState({
-          movieSearchList: results,
-          totalResults,
-          page,
-          onloadingSearch: false,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          onFailSearch: error,
-          onloadingSearch: false,
-        });
-      });
-    }
-  }, 300);
-
-  componentDidMount() {
-    this.updateMovieList("return", 1);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { searchWord, page } = this.state;
-
-    if (
-      (prevState.searchWord !== searchWord && searchWord) ||
-      prevState.page !== page
-    ) {
-      this.updateMovieList(searchWord, page);
-    }
-  }
-
-  changeSearchWord = (searchWord) => {
-    if (searchWord !== "" && searchWord.match(/[\S]/) !== null) {
-      this.setState({
+    state = {
+        movieSearchList: [],
         onloadingSearch: true,
-      });
+        onFailSearch: false,
+        searchWord: '',
+        page: 1,
+        totalResults: null,
+        scroll: false,
+    };
+
+    updateMovieList = _.debounce((searchWord, searchPage) => {
+        if (searchWord !== '' && searchWord.match(/[\S]/) !== null) {
+            MovieService.getMoviesByTitle(searchWord, searchPage)
+                .then(({ results, total_results: totalResults, page }) => {
+                    this.setState({
+                        movieSearchList: results,
+                        totalResults,
+                        page,
+                        onloadingSearch: false,
+                    });
+                })
+                .catch((error) => {
+                    this.setState({
+                        onFailSearch: error,
+                        onloadingSearch: false,
+                    });
+                });
+        }
+    }, 300);
+
+    componentDidMount() {
+        this.updateMovieList('return', 1);
     }
 
-    this.setState({
-      searchWord,
-      page: 1,
-    });
-  };
+    componentDidUpdate(prevProps, prevState) {
+        const { searchWord, page } = this.state;
 
-  changePage = (page) => {
-    this.setState({
-      page,
-      onloadingSearch: true,
-      scroll: true,
-    });
-    this.scrollToTop();
-  };
+        if (
+            (prevState.searchWord !== searchWord && searchWord) ||
+            prevState.page !== page
+        ) {
+            this.updateMovieList(searchWord, page);
+        }
+    }
 
-  scrollToTop = () => {
-    const { scroll, onloadingSearch } = this.state;
+    changeSearchWord = (searchWord) => {
+        if (searchWord !== '' && searchWord.match(/[\S]/) !== null) {
+            this.setState({
+                onloadingSearch: true,
+            });
+        }
 
-    setTimeout(() => {
-      if (!onloadingSearch && scroll) {
-        document.querySelector('a[href*="#"]').scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
         this.setState({
-          scroll: false,
+            searchWord,
+            page: 1,
         });
-      } else {
+    };
+
+    changePage = (page) => {
+        this.setState({
+            page,
+            onloadingSearch: true,
+            scroll: true,
+        });
         this.scrollToTop();
-      }
-    }, 250);
-  };
+    };
 
-  render() {
-    const {
-      movieSearchList,
-      onloadingSearch,
-      onFailSearch,
-      searchWord,
-      page,
-      totalResults,
-      workMode,
-    } = this.state;
+    scrollToTop = () => {
+        const { scroll, onloadingSearch } = this.state;
 
-    const loadContent = onloadingSearch ? (
-      <Spin tip="Loading..." size="large" />
-    ) : (
-      <MovieList
-        movieSearchList={movieSearchList}
-        page={page}
-        totalResults={totalResults}
-        changePage={this.changePage}
-        workMode={workMode}
-      />
-    );
+        setTimeout(() => {
+            if (!onloadingSearch && scroll) {
+                document.querySelector('a[href*="#"]').scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                });
+                this.setState({
+                    scroll: false,
+                });
+            } else {
+                this.scrollToTop();
+            }
+        }, 250);
+    };
 
-    if (onFailSearch) {
-      return <ErrorMessage error={onFailSearch} />;
+    render() {
+        const {
+            movieSearchList,
+            onloadingSearch,
+            onFailSearch,
+            searchWord,
+            page,
+            totalResults,
+            workMode,
+        } = this.state;
+
+        const loadContent = onloadingSearch ? (
+            <Spin tip='Loading...' size='large' />
+        ) : (
+            <MovieList
+                movieSearchList={movieSearchList}
+                page={page}
+                totalResults={totalResults}
+                changePage={this.changePage}
+                workMode={workMode}
+            />
+        );
+
+        const isError = onFailSearch ? (
+            <ErrorMessage error={onFailSearch} />
+        ) : null;
+
+        // if (onFailSearch) {
+        //   return <ErrorMessage error={onFailSearch} />;
+        // }
+
+        return (
+            <ContextProvider>
+                <div className='app'>
+                    <Tabs />
+                    <Search
+                        searchWord={searchWord}
+                        changeSearchWord={this.changeSearchWord}
+                    />
+                    {loadContent}
+                    {isError}
+                </div>
+            </ContextProvider>
+        );
     }
-
-    return (
-      <ContextProvider>
-        <div className="app">
-          <Tabs />
-          <Search
-            searchWord={searchWord}
-            changeSearchWord={this.changeSearchWord}
-          />
-          {loadContent}
-        </div>
-      </ContextProvider>
-    );
-  }
 }
 
 export default App;
--- moviesApp/src/components/errorMessage/ErrorMessage.jsx
+++ moviesApp/src/components/errorMessage/ErrorMessage.jsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Alert } from "antd";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from 'antd';
 
-const ErrorMessage = (error) => (
-  <Alert
-    className="error"
-    type="error"
-    message={`Error: Can't download data! ${error}`}
-    description="An error occurred while downloading data from a remote server"
-    banner
-  />
+const ErrorMessage = ({ error }) => (
+    <Alert
+        className='error'
+        type='error'
+        message={`Error: Can't download data! ${error}`}
+        description='An error occurred while downloading data from a remote server'
+        banner
+    />
 );
+
+ErrorMessage.propTypes = {
+    error: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default ErrorMessage;
